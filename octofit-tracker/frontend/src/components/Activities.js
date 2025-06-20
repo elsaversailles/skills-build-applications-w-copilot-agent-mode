32fab7ee5df93e6b249a07f76a83f9ab54f833e2
Vince Versailles <elsaversailles76@gmail.com>
2025-06-20 08:19:17 +0000

    Updated components to include Codespace URLs for API endpoints

---
+++ octofit-tracker/frontend/src/components/Activities.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+
+function Activities() {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    fetch('https://orange-funicular-pp47wj74x4wf6jxr-8000.app.github.dev/api/activities')
+      .then(response => response.json())
+      .then(data => setActivities(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Activities</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Description</th>
+          </tr>
+        </thead>
+        <tbody>
+          {activities.map(activity => (
+            <tr key={activity.id}>
+              <td>{activity.id}</td>
+              <td>{activity.name}</td>
+              <td>{activity.description}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Activities;
---
+++ octofit-tracker/frontend/src/components/Users.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+
+function Users() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('https://orange-funicular-pp47wj74x4wf6jxr-8000.app.github.dev/api/users')
+      .then(response => response.json())
+      .then(data => setUsers(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Users</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(user => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Users;
